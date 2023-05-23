f4cc416064d1d993076518642672ed83795278c8
Akhil <sharma.akhil484@gmail.com>
2023-05-23 18:25:15 +0530

    added collisions

--- game/code.py
+++ game/code.py
@@ -9,8 +9,8 @@ FPS = 60
 FramePerSec = pygame.time.Clock()
 
 # Predefined some colors
-BLUE  = (0, 0, 255)
-RED   = (255, 0, 0)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -22,29 +22,28 @@ SCREEN_HEIGHT = 500
 DISPLAYSURF = pygame.display.set_mode((500, 500))
 DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Game")
-
+game_running = True
 
 class Ball(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("newball.png")
+        self.image = pygame.image.load("circle.png")
         self.rect = self.image.get_rect()
         self.rect.center = (250, 250)
-        self.speed_x = 0
-        self.speed_y = 5
+        self.speed = [5, 5]
 
     def move(self):
-        self.rect.x += self.speed_x
-        self.rect.y += self.speed_y
+        self.rect.x += self.speed[0]
+        self.rect.y += self.speed[1]
+        if self.rect.bottom > 487 or self.rect.top < 20:
+        	game_running = False
 
-        if self.rect.bottom > 600 or self.rect.top < 0:
-            self.speed_y *= -1
         if self.rect.right > SCREEN_WIDTH or self.rect.left < 0:
-            self.speed_x *= -1
+        	self.speed[0] *= -1
 
     def reverse_direction(self):
-        self.speed_y *= -1
-        self.speed_x *= -1
+        # self.speed[0] *= -1
+        self.speed[1] *= -1
 
     def draw(self, surface):
         surface.blit(self.image, self.rect)
@@ -53,7 +52,7 @@ class Ball(pygame.sprite.Sprite):
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.image = pygame.image.load("sledg.png")
+        self.image = pygame.image.load("rectangle.png")
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
@@ -70,31 +69,31 @@ class Player(pygame.sprite.Sprite):
         surface.blit(self.image, self.rect)
 
 
-P1 = Player(250, 20)
-P2 = Player(250, 480)
-b = Ball()
+def play_game():
+	P1 = Player(250, 20)
+	P2 = Player(250, 480)
+	b = Ball()
 
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-    
-    P1.update()
-    P2.update()
-    b.move()
+	while game_running:
+	    for event in pygame.event.get():
+	        if event.type == QUIT:
+	            pygame.quit()
+	            sys.exit()
 
-    # Collision detection
-    if b.rect.colliderect(P1.rect) or b.rect.colliderect(P2.rect):
-        b.reverse_direction()
-    
-    DISPLAYSURF.fill(BLUE)
-    # P1.draw(DISPLAYSURF)
-    # P2.draw(DISPLAYSURF)
-    pygame.draw.rect(DISPLAYSURF, BLACK, P1.rect)  # Draw P1's rect
-    pygame.draw.rect(DISPLAYSURF, BLACK, P2.rect)
-    # pygame.draw.rect(DISPLAYSURF, BLACK, b.rect)
-    b.draw(DISPLAYSURF)
+	    P1.update()
+	    P2.update()
+	    b.move()
 
-    pygame.display.update()
-    FramePerSec.tick(FPS)
+	    # Collision detection
+	    if b.rect.colliderect(P1.rect) or b.rect.colliderect(P2.rect):
+	        b.reverse_direction()
+
+	    DISPLAYSURF.fill(BLUE)
+	    P1.draw(DISPLAYSURF)
+	    P2.draw(DISPLAYSURF)
+	    b.draw(DISPLAYSURF)
+
+	    pygame.display.update()
+	    FramePerSec.tick(FPS)
+
+play_game()
